eada99288cfa6123dddb7ebaa5253f5e9a9948d4
ANASTAD2000 <anasbaclibr@gmai.com>
2025-02-20 17:55:15 +0100

    add servlet patient

--- src/main/java/com/doctorrv/model/Patient.java
+++ src/main/java/com/doctorrv/model/Patient.java
@@ -1,5 +1,61 @@
+ 
 package com.doctorrv.model;
 
 public class Patient {
+    private int id;
+    private String username;
+    private String email;
+    private String telephone;
+    private String password;
 
+    // Constructor
+    public Patient(int id, String username, String email, String telephone, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.telephone = telephone;
+        this.password = password;
+    }
+
+    // Getters (to get data)
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters (to update data)
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/doctorrv/util/DatabaseConnection.java
+++ src/main/java/com/doctorrv/util/DatabaseConnection.java
@@ -1,5 +1,31 @@
 package com.doctorrv.util;
-  
-public class   DatabaseConnection {
+     
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // Database URL
+    private static final String URL = "jdbc:mysql://localhost:8089/Xdoctorrv"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "admin"; 
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            // Establish the connection
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection to the database successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+        return connection;
+    }
 }
+
+
+
+
